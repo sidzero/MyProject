5dbe7352757c97586d1e68a96096eb7cc59a0212
Sidzzero <srikanth.siddhu@gmail.com>
2015-08-16 19:34:56 +0530

    practising file processing

---
+++ ZeroEngine/simple/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<string>
+#include<cstdlib>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ofstream outputFile;
+	ifstream inputFile;
+	/*
+	outputFile.open("myfile.txt");
+	if (outputFile.fail())
+	{
+		perror("myfile.txt");
+		return -1;
+	}
+	outputFile << "Hello World";
+	outputFile.close();*/
+	
+	inputFile.open("myfile.txt");
+
+	if (inputFile.fail())
+	{
+		perror("myfile.txt");
+		system("PAUSE");
+		return -1;
+	}
+
+	
+	string input;
+	
+	//inputFile >> input;
+	while(getline(inputFile,input))
+	{ 
+	
+	cout <<input <<endl;
+	}
+	system("PAUSE");
+	return 0;
+}
